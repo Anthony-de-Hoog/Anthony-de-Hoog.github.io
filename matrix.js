953b97987ecfd35b400d32c42458dd4569197633
Anthony-de-Hoog <37657@ma-web.nl>
2025-03-22 15:48:07 +0100

    More fun effects

--- matrix.js
+++ matrix.js
@@ -4,30 +4,28 @@ document.addEventListener("DOMContentLoaded", function() {
 
     function type(element, text, index) {
         if (index < text.length) {
-            element.textContent += text.charAt(index);
-            setTimeout(() => type(element, text, index + 1), 100); // Adjust typing speed here
+            element.innerHTML = text.substring(0, index + 1) + '<span class="typing-cursor">|</span>';
+            setTimeout(() => type(element, text, index + 1), 20); // Adjusted typing speed here
         } else {
+            element.innerHTML = text; // Remove typing cursor after typing is done
             if (currentElementIndex < elements.length - 1) {
-                element.parentElement.classList.remove("typing-active");
                 currentElementIndex++;
                 startTyping(elements[currentElementIndex]);
             } else {
-                element.parentElement.classList.add("typing-active");
+                element.innerHTML = text + '<span class="cursor"></span>'; // Add blinking cursor to the last element
             }
         }
     }
 
     function startTyping(element) {
         const text = element.getAttribute("data-text");
-        element.textContent = "";
-        element.parentElement.classList.add("typing-active");
+        element.innerHTML = ''; // Clear the element content
         type(element, text, 0);
     }
 
     elements.forEach(element => {
         element.setAttribute("data-text", element.textContent);
-        element.textContent = "";
-        element.parentElement.classList.remove("typing-active");
+        element.innerHTML = "";
     });
 
     if (elements.length > 0) {
